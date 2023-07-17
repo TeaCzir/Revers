ac27397c1d141c1cd89216e277dd1561c0120188
TeaCzir <teaczir@gmail.com>
2023-07-18 00:29:32 +0300

    Инициализация

---
+++ ReArr/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Разворот массива в обратном порядке с использованием 
+ временной переменной temp.*/
+Console.Clear();
+int[] a = { 8, 0, 12, 5, 9, 32, 85 };
+Console.Write("  Массив: { ");
+for (int i = 0; i < 7; i++)
+{
+   Console.Write($"{a[i]}, ");
+}
+Console.Write("}");
+Console.WriteLine();
+Console.Write("Разворот: { ");
+int temp = 6;
+for (int i = 0; temp >= i; temp--)
+{
+   Console.Write($"{a[temp]}, ");
+}
+Console.Write("}");
---
+++ ReArr_01/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Разворот массива в обратном порядке.*/
+Console.Clear();
+int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+int size = 7;
+Console.Write("  Массив: { ");
+for (int i = 0; i < size; i++)
+{
+   Console.Write($"{a[i]}, ");
+}
+Console.Write("}");
+Console.WriteLine();
+Console.Write("Разворот: { ");
+for (int i = size - 1; i >= 0; i--)
+Console.Write($"{a[i]}, ");
+Console.Write("}");
